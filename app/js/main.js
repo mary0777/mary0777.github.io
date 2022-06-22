cf8381a198f9884f2cc9f89a94e0707484657451
mary0777 <maksymenko.m.07@gmail.com>
2022-06-22 21:47:29 +0300

    fixed bags, added: features, restaurants, reviews, app

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,13 @@
-
-
 $(function () {
 
   
-  var mixer = mixitup('.categories__list',{
-   
-   
+  $('.reviews__slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick__btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick__btn slick-next"></button>'
   });
 
+  var mixer = mixitup('.categories__list',{ });
   mixer.filter('.category-burger');
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,8 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
@@ -102,4 +104,5 @@ exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
 exports.default = parallel(styles, scripts, browsersync, watching, svgSprites);
+
 
